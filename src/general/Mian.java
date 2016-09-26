1fcf1939b5ed8780a67427e5a55675ff8fd9db2c
Adiel <adielbot@gmail.com>
2016-09-26 16:18:12 +0300

    third!!!!

--- src/general/Mian.java
+++ src/general/Mian.java
@@ -5,5 +5,6 @@ public class Mian {
 	public static void main(String[] args) {
 		System.out.println("hello git !");
 		System.out.println("hello num 2!!!");
+		System.out.println("hello num 3!!!");
 	}
 }
